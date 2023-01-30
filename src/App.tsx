4317ce7c4e47214df1f92bdcced0efb6f8ff1955
Jared Lim <jorlim7@uw.edu>
2023-01-30 14:58:22 -0800

    Typescript working

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage2 from './features/HomePage/HomePage2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <HomePage2 />
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    </>
+
   );
 }
 
---
+++ src/features/HomePage/HomePage2.tsx
@@ -0,0 +1,14 @@
+
+
+
+const HomePage2: React.FC = () => {
+
+    return (
+        <h1>Hi</h1>
+        
+    )
+
+}
+
+
+export default HomePage2
