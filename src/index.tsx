a3ee21cd23e48f1a06178207e6a0842cf6b36058
Jared Lim <jorlim7@uw.edu>
2023-01-30 14:47:15 -0800

    tried to set up firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat/app'
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzmolqh8q8BBGWrZKBoxW-sOjCa6Uihzg",
+  authDomain: "future5-7a3d2.firebaseapp.com",
+  projectId: "future5-7a3d2",
+  storageBucket: "future5-7a3d2.appspot.com",
+  messagingSenderId: "437088556082",
+  appId: "1:437088556082:web:ba572c17fd9b1d4200182b"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+  
+firebase.initializeApp(firebaseConfig)
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
+
 root.render(
   <React.StrictMode>
     <App />
