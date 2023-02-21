384f823b27731723af187b4b77b8732d9504c949
Jared Lim <jorlim7@uw.edu>
2023-02-20 22:51:26 -0800

    fixed web app

--- src/App.tsx
+++ src/App.tsx
@@ -23,144 +23,144 @@ const validateEmail = (potentialEmail:string) => {
 
 
 
-const SignInComponent = () => {
-  const firebase = useFirebase()
-  const dispatch = useDispatch()
+// const SignInComponent = () => {
+//   const firebase = useFirebase()
+//   const dispatch = useDispatch()
   
-  useFirebaseConnect({
-    path: "user" // user
-  })
+//   useFirebaseConnect({
+//     path: "user" // user
+//   })
   
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const validEmail = validateEmail(email)
+//   const [name, setName] = useState('')
+//   const [email, setEmail] = useState('')
+//   const [password, setPassword] = useState('')
+//   const validEmail = validateEmail(email)
 
-  const INTERESTS: String[] = ["tennis","children"]
-  const ACCESSIBILITY: String[] = ["vision-impaired", "one-handed"]
+//   const INTERESTS: String[] = ["tennis","children"]
+//   const ACCESSIBILITY: String[] = ["vision-impaired", "one-handed"]
 
-  const cred = {email, password}
+//   const cred = {email, password}
 
-  const MIN_PASSWORD_LENGTH = 5
+//   const MIN_PASSWORD_LENGTH = 5
 
-  const validPassword = password.length >= MIN_PASSWORD_LENGTH
+//   const validPassword = password.length >= MIN_PASSWORD_LENGTH
 
 
   
-  const errors = {
-    email: !validEmail,
-    password: !validPassword
-  }
+//   const errors = {
+//     email: !validEmail,
+//     password: !validPassword
+//   }
   
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // update name
-    setName(event.target.value)
-  }
+//   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     // update name
+//     setName(event.target.value)
+//   }
 
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value)
-  }
+//   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setEmail(event.target.value)
+//   }
   
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value)
-  }
+//   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setPassword(event.target.value)
+//   }
 
 
 
-  const handleSubmit = () => {
-    const profile = {
-      username: name,
-      email: email,
-      interests: INTERESTS,
-      accessibility: ACCESSIBILITY
-    }
-    return firebase
-      .createUser(cred, profile)
-      .then(async (userInfo) => {
-        let uid = userInfo.uid
-      })
-      .catch((error) => {
-        console.log("Error: " + error)
-      })
-  }
+//   const handleSubmit = () => {
+//     const profile = {
+//       username: name,
+//       email: email,
+//       interests: INTERESTS,
+//       accessibility: ACCESSIBILITY
+//     }
+//     return firebase
+//       .createUser(cred, profile)
+//       .then(async (userInfo) => {
+//         let uid = userInfo.uid
+//       })
+//       .catch((error) => {
+//         console.log("Error: " + error)
+//       })
+//   }
 
-  return (
-  <>
-    <FormControl
-      sx={{
-        m: 3,
-        maxWidth: '800px'
-      }}
-      component='fieldset'
-      variant='standard'
-    >
-      <Typography variant='h3'>Create your Code Replayer account</Typography>
-      <Box>
-        <TextField
-          autoComplete='given-name'
-          name='name'
-          fullWidth
-          id='name'
-          label='Name'
-          value={name}
-          onChange={handleNameChange}
-          autoFocus
-          sx={{ m: 1 }}
-        />
-        <TextField
-          required
-          fullWidth
-          id='email'
-          label='Email Address'
-          name='email'
-          value={email}
-          onChange={handleEmailChange}
-          error={errors.email}
-          autoComplete='email'
-          helperText={errors.email && 'input a valid email'}
-          sx={{ m: 1 }}
-        />
-        <TextField
-          required
-          fullWidth
-          value={password}
-          error={errors.password}
-          onChange={handlePasswordChange}
-          name='password'
-          label='Password'
-          type='password'
-          id='password'
-          autoComplete='new-password'
-          helperText={errors.password && 'input a password of at least 8 characters'}
-          sx={{ m: 1 }}
-        />
+//   return (
+//   <>
+//     <FormControl
+//       sx={{
+//         m: 3,
+//         maxWidth: '800px'
+//       }}
+//       component='fieldset'
+//       variant='standard'
+//     >
+//       <Typography variant='h3'>Create your Code Replayer account</Typography>
+//       <Box>
+//         <TextField
+//           autoComplete='given-name'
+//           name='name'
+//           fullWidth
+//           id='name'
+//           label='Name'
+//           value={name}
+//           onChange={handleNameChange}
+//           autoFocus
+//           sx={{ m: 1 }}
+//         />
+//         <TextField
+//           required
+//           fullWidth
+//           id='email'
+//           label='Email Address'
+//           name='email'
+//           value={email}
+//           onChange={handleEmailChange}
+//           error={errors.email}
+//           autoComplete='email'
+//           helperText={errors.email && 'input a valid email'}
+//           sx={{ m: 1 }}
+//         />
+//         <TextField
+//           required
+//           fullWidth
+//           value={password}
+//           error={errors.password}
+//           onChange={handlePasswordChange}
+//           name='password'
+//           label='Password'
+//           type='password'
+//           id='password'
+//           autoComplete='new-password'
+//           helperText={errors.password && 'input a password of at least 8 characters'}
+//           sx={{ m: 1 }}
+//         />
           
-        <Box
-          sx={{
-            my: 4,
-            display: 'flex',
-            justifyContent: 'center'
-          }}
-        >
-          <Button
-            type='submit'
-            size='large'
-            variant='contained'
-            onClick={handleSubmit}
-          >
-            Sign up
-          </Button>
-        </Box>
-      </Box>
-    </FormControl>
-  </>
-  )
-}
+//         <Box
+//           sx={{
+//             my: 4,
+//             display: 'flex',
+//             justifyContent: 'center'
+//           }}
+//         >
+//           <Button
+//             type='submit'
+//             size='large'
+//             variant='contained'
+//             onClick={handleSubmit}
+//           >
+//             Sign up
+//           </Button>
+//         </Box>
+//       </Box>
+//     </FormControl>
+//   </>
+//   )
+// }
 
 function App() {
   return (
     <>
-        <SignInComponent />
+        {/* <SignInComponent /> */}
         <HomePage />
         {/* <div className="App">
           <header className="App-header">
