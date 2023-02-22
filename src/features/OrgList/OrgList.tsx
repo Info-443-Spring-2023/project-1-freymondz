072c706beb94ab7385d3f3a8dbde56be2cd12e74
Audrey Dennis <audreydennis@Audreys-MacBook-Pro-9.local>
2023-02-21 21:07:34 -0800

    Org item UI updates

--- src/features/OrgList/OrgList.tsx
+++ src/features/OrgList/OrgList.tsx
@@ -1,15 +1,33 @@
 import OrgListItem from "./OrgListItem"
-
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
 
 const OrgList: React.FC = () => {
     return (
         <>
             <h3>Organization List:</h3>
-            <h4>Individual Org Items go here:</h4> 
-            {/* TODO: do some sort of mapping here over the list of organizations in our DB */}
-            <OrgListItem />
+            <Paper
+                sx={{
+                    p: 2,
+                    margin: 'auto',
+                    maxWidth: 800,
+                    flexGrow: 1,
+                    backgroundColor: (theme) =>
+                        theme.palette.mode === 'dark' ? '#374785' : '#fff',
+                }}
+            >
+                <Grid container spacing={2}>
+                {/* TODO: do some sort of mapping here over the list of organizations in our DB */}
+                <OrgListItem />
+                </Grid>
+            </Paper>
         </>
     )
 }
 
 export default OrgList
+
--- src/features/OrgList/OrgListItem.tsx
+++ src/features/OrgList/OrgListItem.tsx
@@ -3,6 +3,19 @@ import { useFirebase } from "react-redux-firebase"
 import { RootState } from "../../store"
 import { selectOrgId, setOrgId } from "./OrgsSlice"
 
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+
+const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+});
+
 //TODO: rename this to positionListItem
 const OrgListItem: React.FC = () => {
 
@@ -17,12 +30,36 @@ const OrgListItem: React.FC = () => {
     }
     return (
         <>
-            {/* TODO: render one individual org list item. */}
-            <h5 onClick={onClick}>I'm Seattle Promise</h5>
-            {/* do this as a MUI button or something */}
-            <h5>Interests go here </h5>
-             {/* do this as a MUI button or something */}
-             <h5>Accessibilities go here </h5>
+            <Grid item>
+                <ButtonBase sx={{ width: 128, height: 128 }}>
+                    <Img alt="Org Image" src="/static/images/grid/complex.jpg" /> 
+                </ButtonBase>
+            </Grid>
+            <Grid item xs={12} sm container>
+                <Grid item xs container direction="column" spacing={2}>
+                    <Grid item xs>
+                        <Typography gutterBottom variant="subtitle1" component="div">
+                            Position Title
+                        </Typography>
+                        <Typography variant="body2" gutterBottom>
+                            Organization Name
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                            Click to show more info
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Typography variant="subtitle1" component="div">
+                        Rating: 5 stars
+                    </Typography>
+                </Grid>
+            </Grid>
         </>
     )
 }
