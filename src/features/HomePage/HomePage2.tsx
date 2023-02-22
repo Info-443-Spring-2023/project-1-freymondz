38bf52ce28f52cf919f2c2c9c2c908af8cd3793c
Audrey Dennis <audreydennis@Audreys-MacBook-Pro-9.local>
2023-02-21 19:50:02 -0800

    Added navbar

--- src/features/HomePage/HomePage2.tsx
+++ src/features/HomePage/HomePage2.tsx
@@ -6,7 +6,6 @@ import OrgList from "../OrgList/OrgList"
 const HomePage: React.FC = () => {
     return (
         <>
-            <h1>Hi</h1>
             <NavBar/>
             <OrgList/>
                    
