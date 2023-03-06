c81964ecea03dc533bdab10e44433f64ed4ae09f
Duy Hoang Nguyen <duyhoangnguyen2000@gmail.com>
2023-03-06 13:18:59 -0800

    added styling

--- src/features/AboutPage/AboutPage.tsx
+++ src/features/AboutPage/AboutPage.tsx
@@ -1,18 +1,15 @@
 import * as React from 'react';
-import { Box, CardMedia, Typography } from '@mui/material';
+import { Box, CardMedia, List, ListItem, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles'
 import { blue, green, red } from '@mui/material/colors';
 
 const AboutPage: React.FC = () => {
 
-    const Root = styled('div')(({ theme }) => ({
+    const Video = styled('iframe')(({ theme }) => ({
         padding: theme.spacing(1),
-        [theme.breakpoints.down('md')]: {
-            backgroundColor: red[500],
-        },
-        [theme.breakpoints.down('lg')]: {
-            width: "853",
-            height: "480"
+        [theme.breakpoints.up('lg')]: {
+            width: 1200,
+            height: 720
         }
     }));
 
@@ -33,47 +30,47 @@ const AboutPage: React.FC = () => {
                     </b>
                 </Typography>
 
-                <Typography>
-                    <ul>
+                
+                {/* if you want to use unorder list, please follow this format to 
+                    reduce error
+                    <List sx={{ listStyleType: 'disc', pl: 4 }} >
+                        <ListItem sx={{ display: 'list-item' }}>
+                    </ListItem>
+                </List> */}
 
-                    </ul>
-                </Typography>
 
                 <Typography variant="body1">Volunteering is a key importance to the development of our society & communities. Through volunteering, everyone is able to learn aout global matters & help make a change not only within their community but through the world too. Our website "FutureForward" allows our younger audience of studenst to be able to be more knowledgable on ways of helping in different sectors near them. Through our website "FutureForwrd", students can :  </Typography>
+                <List sx={{ listStyleType: 'disc', pl: 4 }} >
+                    <ListItem sx={{ display: 'list-item' }}>
+                        Register & create a personalized account.
+                    </ListItem>
 
-                <Typography variant="body1">
-                    <ul>
-                        <li>
-                            Register & create a personalized account.
-                        </li>
+                    <ListItem sx={{ display: 'list-item' }}>
+                        Find volunteer oppourtunities based on filtered interests.
+                    </ListItem>
 
-                        <li>
-                            Find volunteer oppourtunities based on filtered interests.
-                        </li>
+                    <ListItem sx={{ display: 'list-item' }}>
+                        Bookmark volunteer oppourtunities to be saved and seen in personal profiles
+                    </ListItem>
 
-                        <li>
-                            Bookmark volunteer oppourtunities to be saved and seen in personal profiles
-                        </li>
+                    <ListItem sx={{ display: 'list-item' }}>
+                        Have accurate information on accessible volunteer oppourtunities.
+                    </ListItem>
 
-                        <li>
-                            Have accurate information on accessible volunteer oppourtunities.
-                        </li>
-
-                        <li>
-                            Save service information done within their personalized profiles.
-                        </li>
-                    </ul>
-                </Typography>
+                    <ListItem sx={{ display: 'list-item' }}>
+                        Save service information done within their personalized profiles.
+                    </ListItem>
+                </List>
 
                 <Typography>
                     When developing this website for youth, we had in mind developing sustainable cities & communities to be able to make them more inclusive, safe, resilient and sustainable. The youth are the future of these cities & communities & therfore motivating them to create a sustainable city through volunteering is key for the future.
                 </Typography>
 
-                <Typography>
+                {/* <Typography>
                     <ul>
 
                     </ul>
-                </Typography>
+                </Typography> */}
 
                 <Typography variant="body1">
                     The following TedX video presents speaker, 13 year old Kofoworola Jolaoso, as she talks about the importance of allowing and letting kids start volunteering at a young age. The main problem that Jolaoso states is that the problem isnt the lack of interest but the lack of knowledge in helping & see what is avialabe to help in.
@@ -85,16 +82,12 @@ const AboutPage: React.FC = () => {
                 alignItems="center"
                 paddingBottom={5}>
                 <div className="video-responsive">
-                    <Root>
-                        <iframe
-                            // width="853"
-                            // height="480"
+                        <Video
                             src={`https://www.youtube.com/embed/fDocxsO2tYA`}
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
                             title="Embedded youtube"
                         />
-                    </Root>
                 </div>
             </Box>
 
