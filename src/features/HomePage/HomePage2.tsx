1cdfa35abd0349662922b1171a74d8ea7d4cb927
freymond <37636251+freymondz@users.noreply.github.com>
2023-04-27 20:23:23 -0700

    remove uneededd placeholder jsx elements

--- src/features/HomePage/HomePage2.tsx
+++ src/features/HomePage/HomePage2.tsx
@@ -30,74 +30,70 @@ const HomePage: React.FC = () => {
 
     if (small) {
         return (
-            <>
-                <Stack max-width="700px" data-testid="small"
-                >
-                    {!open ?
-                        <Button id='filterShowcase'
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                            variant="contained"
-                            onClick={handleHideFilter}
-                        >
-                            Show Filter
-                        </Button>
+            <Stack max-width="700px" data-testid="small"
+            >
+                {!open ?
+                    <Button id='filterShowcase'
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                        variant="contained"
+                        onClick={handleHideFilter}
+                    >
+                        Show Filter
+                    </Button>
 
-                        :
-                        <Button
-                            id='filterShowcase'
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                            variant="contained"
+                    :
+                    <Button
+                        id='filterShowcase'
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                        variant="contained"
 
-                            onClick={handleHideFilter}
-                        >
-                            Hide Filter
-                        </Button>
+                        onClick={handleHideFilter}
+                    >
+                        Hide Filter
+                    </Button>
 
-                    }
+                }
 
-                    {open ?
-                        <FilterBar />
-                        :
-                        <></>}
-                    <OrgList />
-                </Stack>
-            </>
+                {open ?
+                    <FilterBar />
+                    :
+                    <></>}
+                <OrgList />
+            </Stack>
         )
     }
     return (
-        <>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} marginTop={6} marginLeft={4} marginRight={4} paddingBottom={6}>
-                <Grid item xs={3} marginLeft={4}>
-                    {!open ?
-                        <Button
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                            variant="contained"
-                            onClick={handleHideFilter}
-                        >
-                            Show Filter
-                        </Button>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} marginTop={6} marginLeft={4} marginRight={4} paddingBottom={6}>
+            <Grid item xs={3} marginLeft={4}>
+                {!open ?
+                    <Button
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                        variant="contained"
+                        onClick={handleHideFilter}
+                    >
+                        Show Filter
+                    </Button>
 
-                        :
-                        <Button
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                            variant="contained"
+                    :
+                    <Button
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                        variant="contained"
 
-                            onClick={handleHideFilter}
-                        >
-                            Hide Filter
-                        </Button>
+                        onClick={handleHideFilter}
+                    >
+                        Hide Filter
+                    </Button>
 
-                    }
-                    {open ?
-                        <FilterBar />
-                        :
-                        <></>}
-                    </Grid>
-                <Grid item xs={8}>
-                    <OrgList />
+                }
+                {open ?
+                    <FilterBar />
+                    :
+                    <></>}
                 </Grid>
+            <Grid item xs={8}>
+                <OrgList />
             </Grid>
-        </>
+        </Grid>
     )
 }
 
