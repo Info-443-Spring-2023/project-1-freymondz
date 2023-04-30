593b314bfae261eddee66c67cc3c364bbe7e27ba
Zachary <37636251+freymondz@users.noreply.github.com>
2023-04-29 19:28:42 -0700

    Refactor `HomePage2.tsx` (#7)
    * remove unused code comments
    
    * clean up conditional statement in useEffect
    
    * remove uneeded else branch
    
    * remove uneededd placeholder jsx elements
    
    * fix inconsistent variable formatting
    
    * use prettier to make for consitent style
    
    * remove duplicate JSX elements

--- src/features/HomePage/HomePage2.tsx
+++ src/features/HomePage/HomePage2.tsx
@@ -1,116 +1,69 @@
-import * as React from 'react';
-import OrgList from "../Positions/PositionList"
+import * as React from "react";
+import OrgList from "../Positions/PositionList";
 import FilterBar from "../FilterBar/FilterBar";
-import Grid from '@mui/material/Grid';
-import { Button, Stack } from '@mui/material';
-
-
+import Grid from "@mui/material/Grid";
+import { Button, Stack } from "@mui/material";
 
 const HomePage: React.FC = () => {
-
-    const [open, setOpen] = React.useState<boolean>(false)
+    const [open, setOpen] = React.useState<boolean>(false);
     const handleHideFilter = () => {
-        setOpen(!open)
-    }
+        setOpen(!open);
+    };
 
-
-    const [small, setSmall] = React.useState<boolean>(false)
+    const [small, setSmall] = React.useState<boolean>(false);
 
     React.useEffect(() => {
         const resize = () => {
-            const mq_small = window.matchMedia("(max-width: 768px)")
-            if (mq_small.matches) {
-                setSmall(true)
-            } else {
-                setSmall(false)
-            }
-        }
+            const mediaQuery = window.matchMedia("(max-width: 768px)");
+            setSmall(mediaQuery.matches);
+        };
 
-        window.addEventListener('resize', resize)
-        
-        return () => window.removeEventListener('resize', resize)
-    }, [])
+        window.addEventListener("resize", resize);
 
+        return () => window.removeEventListener("resize", resize);
+    }, []);
+
+    const filterButton = (
+        <Button
+            id="filterShowcase"
+            sx={{ my: 2, color: "white", display: "block" }}
+            variant="contained"
+            onClick={handleHideFilter}
+        >
+            {!open ? "Show Filter" : "Hide Filter"}
+        </Button>
+    );
+
+    const filterBar = open ? <FilterBar /> : <></>;
 
     if (small) {
         return (
-            <>
-                <Stack max-width="700px" data-testid="small"
-                // container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} marginTop={6} marginLeft={4} marginRight={4} paddingBottom={6}
-                >
-                    {/* <Item xs={3} marginLeft={4}> */}
-                    {!open ?
-                        <Button id='filterShowcase'
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                            variant="contained"
-                            onClick={handleHideFilter}
-                        >
-                            Show Filter
-                        </Button>
-
-                        :
-                        <Button
-                            id='filterShowcase'
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                            variant="contained"
-
-                            onClick={handleHideFilter}
-                        >
-                            Hide Filter
-                        </Button>
-
-                    }
-
-                    {open ?
-                        <FilterBar />
-                        :
-                        <></>}
-                    {/* </Item> */}
-                    {/* <Grid item xs={8}> */}
-                    <OrgList />
-                    {/* </Grid> */}
-                </Stack>
-            </>
-        )
-    } else {
-        return (
-            <>
-                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} marginTop={6} marginLeft={4} marginRight={4} paddingBottom={6}>
-                    <Grid item xs={3} marginLeft={4}>
-                        {!open ?
-                            <Button
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                                variant="contained"
-                                onClick={handleHideFilter}
-                            >
-                                Show Filter
-                            </Button>
-
-                            :
-                            <Button
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                                variant="contained"
-
-                                onClick={handleHideFilter}
-                            >
-                                Hide Filter
-                            </Button>
-
-                        }
-                        {open ?
-                            <FilterBar />
-                            :
-                            <></>}
-                        </Grid>
-                    <Grid item xs={8}>
-                        <OrgList />
-                    </Grid>
-                </Grid>
-            </>
-        )
+            <Stack max-width="700px" data-testid="small">
+                {filterButton}
+                {filterBar}
+                <OrgList />
+            </Stack>
+        );
     }
+    return (
+        <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            marginTop={6}
+            marginLeft={4}
+            marginRight={4}
+            paddingBottom={6}
+        >
+            <Grid item xs={3} marginLeft={4}>
+                {filterButton}
+                {filterBar}
+            </Grid>
+            <Grid item xs={8}>
+                <OrgList />
+            </Grid>
+        </Grid>
+    );
+};
 
-}
-
-
-export default HomePage
+export default HomePage;
